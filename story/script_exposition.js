db030d3c1a314c4a6f8b84ab96679c0a3936b25e
Sunny Lee <slee6390@gmail.com>
2023-08-19 01:39:39 -0400

    Tutorial

--- story/script_exposition.js
+++ story/script_exposition.js
@@ -1,54 +1,56 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const choice1Button = document.getElementById("choice1")
-    const choice2Button = document.getElementById("choice2")
-    const choice3Button = document.getElementById("choice3")
-    const choice4Button = document.getElementById("choice4")
-    const choice5Button = document.getElementById("choice5")
-    const monthText = document.getElementById("month")
+<script>
 
-    let currentWeek = 1
+    document.addEventListener("DOMContentLoaded", function() {
+        const choice1Button = document.getElementById("choice1")
+        const choice2Button = document.getElementById("choice2")
+        const choice3Button = document.getElementById("choice3")
+        const choice4Button = document.getElementById("choice4")
+        const choice5Button = document.getElementById("choice5")
+        const monthText = document.getElementById("month")
 
-    function updateWeek() {
-        monthText.textContent = currentMonth
-    }
-    
-    function handleChoice(choice) {
-        resultText.textContent = `You chose ${choice}. This affects your character's development in Month ${currentMonth}.`
-    }
-    choice1Button.addEventListener("click", function() {
-        handleChoice("Option 1")
-    })
+        let currentWeek = 1
 
-    choice2Button.addEventListener("click", function() {
-        handleChoice("Option 2")
-    })
+        function updateWeek() {
+            monthText.textContent = currentMonth
+        }
+        
+        function handleChoice(choice) {
+            resultText.textContent = `You chose ${choice}. This affects your character's development in Month ${currentMonth}.`
+        }
+        choice1Button.addEventListener("click", function() {
+            handleChoice("Option 1")
+        })
 
-    choice3Button.addEventListener("click", function() {
-        handleChoice("Option 3")
-    })
+        choice2Button.addEventListener("click", function() {
+            handleChoice("Option 2")
+        })
 
-    choice4Button.addEventListener("click", function() {
-        handleChoice("Option 4")
-    })
+        choice3Button.addEventListener("click", function() {
+            handleChoice("Option 3")
+        })
 
-    choice5Button.addEventListener("click", function() {
-        handleChoice("Option 5")
-    })
+        choice4Button.addEventListener("click", function() {
+            handleChoice("Option 4")
+        })
 
-    // Update the week and content for the next week
-    function goToNextMonth() {
-        currentMonth++
+        choice5Button.addEventListener("click", function() {
+            handleChoice("Option 5")
+        })
+
+        // Update the week and content for the next week
+        function goToNextMonth() {
+            currentMonth++
+            updateMonth()
+            resultText.textContent = ""
+        }
+
+        // Trigger the next week after a choice is made
+        choice1Button.addEventListener("click", goToNextMonth)
+        choice2Button.addEventListener("click", goToNextMonth)
+
+        // Initialize the week display
         updateMonth()
-        resultText.textContent = ""
-    }
-
-    // Trigger the next week after a choice is made
-    choice1Button.addEventListener("click", goToNextMonth)
-    choice2Button.addEventListener("click", goToNextMonth)
-
-    // Initialize the week display
-    updateMonth()
-})
-
+    })
+</script>
 
 
